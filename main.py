ce6291b6079cf469d9288d5c5cd17a4d0054d0a9
Aditya Chauhan <adi9969584499@gmail.com>
2025-07-07 06:22:18 +0530

    Added Named Entity Recognition (NER) using spaCy with custom SKILL entity

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import spacy
 
+# Cache Spacy
+@st.cache_resource
+def load_spacy_model():
+    return spacy.load("en_core_web_trf")
+nlp=load_spacy_model()
+
 # Extracting Text From a Pdf File
 
 def extract_text_from_pdf(pdf_file):
@@ -62,13 +68,55 @@ if uploaded_files and job_description:
         'python', 'sql', 'excel', 'power bi', 'tableau', 'r', 'pandas', 'numpy',
         'scikit-learn', 'matplotlib', 'seaborn', 'dashboards', 'data cleaning',
         'data visualization', 'machine learning', 'communication', 'statistics',
-        'time series', 'deep learning', 'nlp', 'big data', 'aws', 'azure'
+        'time series', 'deep learning', 'nlp', 'big data', 'aws', 'azure',"Git",
+        "Jupyter", "Excel", "VLOOKUP","PivotTables","Git, Jupyter, Excel","VLOOKUP"
         ]
 
-        # Define the Skill Extractor
+        # Named Entity Recognition
 
-        import spacy
-        nlp=spacy.load("en_core_web_sm")
+        from spacy.pipeline import EntityRuler
+
+        ruler=nlp.add_pipe("entity_ruler",before="ner")
+        patterns=[{"label":"SKILL", "pattern":skill} for skill in known_skills]
+        ruler.add_patterns(patterns)
+
+        # Define Entity Extractor
+
+        bad_entity_words=set(known_skills) | {
+            "detail", "data", "tableau", "power bi", "excel", "numpy", "matplotlib","Queried"
+        }
+
+        def is_good_entity(ent):
+            txt=ent.text.strip()
+            if txt.lower() in bad_entity_words:                         # If the words are in our bad entity list
+                return False
+            if txt.islower():                                           # If the text is fully lower 
+                return False
+            if len(txt.split())==1 and len(txt)<3:                      # One word entities shorter than 3 characters
+                return False
+            if "\n" in txt or "," in txt:                               # Multi‑line bullets
+                return False
+            return True
+
+        def extract_entities(text):
+            doc=nlp(text)
+            entities={"PERSON":set(), "ORG":set() , "GPE":set(), "DATE":set()}
+
+            for ent in doc.ents:
+                if ent.label_ in entities and is_good_entity(ent):
+                    entities[ent.label_].add(ent.text)
+            
+            return {k: list(v) for k,v in entities.items()}
+        
+        # Get Resume Entity
+
+        resume_entities=[]
+
+        for text in resumes:
+            entities=extract_entities(text)
+            resume_entities.append(entities)
+
+        # Define the Skill Extractor
 
         def extract_skills_from_text(job_description, known_skills):
             job_description=job_description.lower()
@@ -102,6 +150,8 @@ if uploaded_files and job_description:
             resume_skill_matches.append(",".join(matched_required))
             resume_skill_counts.append(f"{len(matched_required)} Out Of {len(required_skills)}")
             resume_hit_rario.append((len(matched_required)/len(required_skills))*100 if required_skills else 0)
+        
+        
 
         # Display Ranked Resumes
 
@@ -110,6 +160,15 @@ if uploaded_files and job_description:
         results["Match Score"]=results["Match Score"]*100
         st.subheader("Ranked Candidates")
         st.dataframe(results,use_container_width=True)
+
+        # Display Named Entities
+
+        st.subheader("Named Entities In Resumes")
+
+        for i, name in enumerate(resume_names):
+            st.markdown(f"***{name}***")
+            with st.expander("View Extracted Entites"):
+                st.json(resume_entities[i], expanded=False)
 
         # Download The Result
         
